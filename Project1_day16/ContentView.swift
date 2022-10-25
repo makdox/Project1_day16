7c475f5eb1649bfefce9d350ae40c181a2578f3a
Maksym Donets <timnirton7@gmail.com>
2022-10-25 11:35:45 -0400

    Initial Commit

---
+++ Project1_day16/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Project1_day16
+//
+//  Created by Max Donets on 25.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project1_day16/Project1_day16App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1_day16App.swift
+//  Project1_day16
+//
+//  Created by Max Donets on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project1_day16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
